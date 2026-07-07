7990d8d4f03b4b499250351e89307b9118541e7b
sunil6374 <sunil6374@rediffmail.com>
2026-07-07 17:59:05 +0530

    Replace GoogleGenerativeAIEmbeddings with OllamaEmbeddings

--- ss_chatbot.py
+++ ss_chatbot.py
@@ -6,7 +6,7 @@ from langchain_community.document_loaders import PyPDFDirectoryLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_classic.chains import create_retrieval_chain
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_ollama import OllamaEmbeddings
 
 
 
@@ -19,8 +19,8 @@ Please provide the most accurate results based on the context.
 Question: {input} """)
 
 groq_api_key = st.secrets["GROQ_API_KEY"]
-google_api_key=st.secrets["GOOGLE_API_KEY"]
-st.session_state.embeddings=GoogleGenerativeAIEmbeddings(model="models/text-embedding-004")
+
+st.session_state.embeddings=OllamaEmbeddings(model='nomic-embed-text:latest')
 st.session_state.loader=PyPDFDirectoryLoader('data')
 st.session_state.docs=st.session_state.loader.load()
 st.session_state.splitter=RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=200)
