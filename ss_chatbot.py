104e10b832451741dca4ec28b98f22e6d6939fc6
sunil6374 <sunil6374@rediffmail.com>
2026-07-07 17:13:24 +0530

    Reformat ss_chatbot.py with consistent imports

--- ss_chatbot.py
+++ ss_chatbot.py
@@ -19,7 +19,7 @@ Please provide the most accurate results based on the context.
 Question: {input} """)
 
 groq_api_key = st.secrets["GROQ_API_KEY"]
-google_api_key=st.secrets["GOOGLE_API_KEY"
+google_api_key=st.secrets["GOOGLE_API_KEY"]
 st.session_state.embeddings=GoogleGenerativeAIEmbeddings(model="models/text-embedding-004")
 st.session_state.loader=PyPDFDirectoryLoader('data')
 st.session_state.docs=st.session_state.loader.load()
